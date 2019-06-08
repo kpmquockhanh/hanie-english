7c740f55ac455f5605f23944a9031ad2ac9b6283
levan <levan.gtvt.97@gmail.com>
2019-06-08 18:07:22 +0700

    Add courses

---
+++ resources/views/admin/courses/index.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layouts.master')
+@section('page-header', 'Phones')
+@section('option-des', 'Danh sách số điện thoại')
+@section('style')
+    <style>
+        td {
+            vertical-align: middle !important;
+        }
+    </style>
+@stop
+@section('content')
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Phone list <a href="{{ route('phones.create') }}" class="btn-sm btn-success" style="margin-left: 5px;"><i class="fa fa-plus"></i></a></h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Phone number</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @if (!$phones->count())
+                    <tr>
+                        <td colspan="5" class="text-center">No data</td>
+                    </tr>
+                @endif
+                @foreach ($phones as $phone)
+                    <tr>
+                        <td>{{ $phone->id }}</td>
+                        <td>{{ $phone->name }}</td>
+                        <td>{{ $phone->phone_number }}</td>
+                        <td>
+                            {{--                                            <a href="{{ route('phones.edit', ['id' => $phone->id]) }}" class="btn-sm btn-success"><i class="fa fa-pencil"></i></a>--}}
+                            <form action="{{ route('phones.destroy', ['id' => $phone->id]) }}" style="display: inline;" method="post">
+                                @csrf
+                                <input type="text" name="_method" value="delete" hidden>
+                                <button type="submit" class="btn-sm btn-danger"><i class="fa fa-trash"></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Phone number</th>
+                    <th>Action</th>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
+        <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+@stop
