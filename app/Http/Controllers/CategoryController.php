9a977636a0060b0fd97287f451e0025f0c0a8631
khanhlq <khanhlq@hblab.vn>
2019-06-08 20:40:03 +0700

    CRUD category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::query()->paginate(5);
+
+        return view('admin.categories.index', compact('categories'));
     }
 
     /**
@@ -24,18 +26,30 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.categories.create');
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request)
     {
-        //
+        $this->validate(
+            $request,
+            [
+                'name' => 'required',
+            ]
+        );
+
+        $createData = $request->only([
+            'name',
+        ]);
+
+        Category::query()->create($createData);
+
+        return redirect(route('categories.index'))->with('success', 'Created successfully!');
     }
 
     /**
@@ -57,19 +71,31 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Category $category
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate(
+            $request,
+            [
+                'name' => 'required',
+            ]
+        );
+
+        $updateData = $request->only([
+            'name',
+        ]);
+
+        Category::query()->update($updateData);
+
+        return redirect(route('categories.index'))->with('success', 'Updated successfully!');
     }
 
     /**
@@ -80,6 +106,11 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        try {
+            $category->delete();
+            return redirect(route('categories.index'))->with('success', 'Deleted successfully!');
+        } catch (\Exception $e) {
+            return redirect(route('categories.index'))->with('error', 'Deleted error!');
+        }
     }
 }
---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="box box-success">
+        <div class="box-header with-border">
+            <h3 class="box-title">{{ ucfirst((request()->segment(3)?request()->segment(3).' ':'').request()->segment(2)) }}</h3>
+        </div>
+        <!-- /.box-header -->
+        <form action="{{ route(request()->segment(2).'.update', ['id' => $category->id]) }}" method="post" class="" enctype="multipart/form-data">
+            <div class="box-body">
+                @include('admin.layouts.error-message')
+                @csrf
+                {{ method_field('PUT') }}
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" placeholder="Name" value="{{ old('name', $category->name) }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                <button type="submit" class="btn btn-info" style="margin-top: 20px; text-align: center">Update</button>
+                <a class="btn btn-warning pull-right" href="{{ route(request()->segment(2).'.index') }}" style="margin: 20px 0 0 10px; text-align: center; ">Back</a>
+            </div>
+        </form>
+    </div>
+@stop
