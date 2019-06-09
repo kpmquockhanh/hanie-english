ba628ba331a5f75682e3fe7dda15e596c14bf3ec
levan <levan.gtvt.97@gmail.com>
2019-06-09 10:26:08 +0700

    UI courses

--- resources/views/admin/courses/create.blade.php
+++ resources/views/admin/courses/create.blade.php
@@ -2,45 +2,49 @@
 @section('page-header', 'Courses')
 @section('option-des', 'Thêm khóa học')
 @section('content')
-    <div class="container">
+    <div class="box box-success">
+        <div class="box-header with-border">
+            <h3 class="box-title">Create courses</h3>
+        </div>
         <form action="{{ route('courses.store') }}" method="post" class="">
             @csrf
-            <div class="row">
-                <div style="display: flex; flex-direction: column; align-items: center;">
-                    @if ($errors->first())
-                        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+            <div class="box-header">
+                <div class="row">
+                    <div style="display: flex; flex-direction: column; align-items: center;">
+                        @if ($errors->first())
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="form-group ic-cmp-int">
+                                    <div class="alert alert-danger">{{ $errors->first() }}</div>
+                                </div>
+                            </div>
+                        @endif
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <div class="form-group ic-cmp-int">
-                                <div class="alert alert-danger">{{ $errors->first() }}</div>
+                                <div class="form-ic-cmp">
+                                    <label for="name">Name</label>
+                                </div>
+                                <div class="nk-int-st">
+                                    <input type="text" name="name" class="form-control" placeholder="Course's Name" value="{{ old('name') }}">
+                                </div>
                             </div>
                         </div>
-                    @endif
-                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
-                        <div class="form-group ic-cmp-int">
-                            <div class="form-ic-cmp">
-                                <i class="notika-icon notika-support"></i>
-                            </div>
-                            <div class="nk-int-st">
-                                <input type="text" name="name" class="form-control" placeholder="Course's Name" value="{{ old('name') }}">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
-                        <div class="form-group ic-cmp-int">
-                            <div class="form-ic-cmp">
-                                <i class="notika-icon notika-next"></i>
-                            </div>
-                            <div class="nk-int-st">
-                                <textarea class="html-editor" id="description" name="description"></textarea>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="form-group ic-cmp-int">
+                                <div class="form-ic-cmp">
+                                    <label for="description">Description</label>
+                                </div>
+                                <div class="nk-int-st">
+                                    <textarea class="html-editor" id="description" name="description"></textarea>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div style="display: flex; justify-content: center">
+            <div class="box-footer">
                 <button type="submit" class="btn btn-success" style="margin-top: 20px; text-align: center">Create</button>
-                <a class="btn btn-warning" href="{{ route('courses.index') }}" style="margin: 20px 0 0 10px; text-align: center; ">Back</a>
+                <a class="btn btn-warning pull-right" href="{{ route(request()->segment(2).'.index') }}" style="margin: 20px 0 0 10px; text-align: center; ">Back</a>
             </div>
-
         </form>
     </div>
 @stop
--- resources/views/admin/courses/show.blade.php
+++ resources/views/admin/courses/show.blade.php
@@ -2,13 +2,19 @@
 @section('page-header', 'Courses')
 @section('option-des', "Thông tin khóa học $course->name")
 @section('content')
-    <div class="container">
+    <div class="box box-success">
+        <div class="box-header with-border">
+            <h3 class="box-title">Information courses</h3>
+        </div>
+        <div class="box-body">
+
+        </div>
         <div class="row">
-            <div class="col-md-12" style="display: flex; justify-content: center">
+            <div class="col-md-12">
                 <div class="col-md-2">
                     <label for="name">Name</label>
                 </div>
-                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
                     <p>{{$course->name}}</p>
                 </div>
             </div>
@@ -16,15 +22,13 @@
                 <div class="col-md-2">
                     <label for="name">Description</label>
                 </div>
-                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
                     <p>{!! $course->description !!}</p>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div style="display: flex; justify-content: center">
-                <a class="btn btn-warning" href="{{ route('courses.index') }}" style="margin: 20px 0 0 10px; text-align: center; ">Back</a>
-            </div>
+        <div class="box-footer">
+            <a class="btn btn-warning" href="{{ route('courses.index') }}" style="margin: 20px 0 0 10px; text-align: center; ">Back</a>
         </div>
     </div>
 @stop
