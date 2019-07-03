264339aab8d77f7a3a3fc0fe1d74cabbeccc6ef8
khanhlq <khanhlq@hblab.n>
2019-07-03 18:19:20 +0700

    Fix url home

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -54,7 +54,7 @@
 
         <div class="container-fluid">
             <div class="d-flex align-items-center">
-                <div class="site-logo mr-auto w-25"><a href="index.html">{{ env('APP_NAME', 'Hanie') }}</a></div>
+                <div class="site-logo mr-auto w-25"><a href="#">{{ env('APP_NAME', 'Hanie') }}</a></div>
 
                 <div class="mx-auto text-center">
                     <nav class="site-navigation position-relative text-right" role="navigation">
