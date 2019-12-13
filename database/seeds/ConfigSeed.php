51c999fd05d9d9ad8030269b929765bdf1bbbadc
khanhlq <khanhlq@hblab.n>
2019-12-13 09:08:03 +0700

    Fix config seeder

--- database/seeds/ConfigSeed.php
+++ database/seeds/ConfigSeed.php
@@ -11,7 +11,7 @@ class ConfigSeed extends Seeder
      */
     public function run()
     {
-        \App\Config::query()->create([
+        \App\Config::query()->insert([
             [
                 'type' => 'text',
                 'show' => true,
